0c39c1f79b520848bbdfd44efb308ccb1d6196df
kraa2005 <85827475+kraa2005@users.noreply.github.com>
2022-01-29 20:41:48 -0800

    the kindness thing added

--- assessment.js
+++ assessment.js
@@ -58,23 +58,25 @@ assessmentButton.onclick = () =>{   //function ()は () =>と書ける そして
   script.setAttribute('src', 'https://platform.twitter.com/widgets.js');
   tweetDivided.appendChild(script);
 };
-const answers = [
 
-    '{userName}`s good point is the voice {userName}s  characteristic voice attracts people and remains in your hearts',
-    '{userName}`s good point is the eyes Those peoplse you look in to must be on ones mind',
-    '{userName}`s good point is the strictness. strictness may seems infuriating, hoever it will lead you to sucsess!',
-    '{userName}`s good point is the knowledge Your smart! HAHAHAHAHAHA',
-    '{userName}`s good point is the uniqueness. You are the only person who can make people laugh!',
-    '{userName}`s good point is the carefulness.your insight will help someone one day',
-    '{userName}`s good point is the look {userName}`s look is fantastic! and attract every people who saw you',
-    '{userName}`s good point is the get-tough. eveytime you make a decision, you are saving someone',
-    '{userName}`s good point is the thoughtfulness {userName} に気をかけてもらった多くの人が感謝しています。',
-    '{userName}`s good point is the sensibility.Everyone can feel your feeling and you can share your emotions with them',
-    '{userName}`s good point is the moderation.everyone around you is appreciating your not too pushy thought',
-    '{userName}`s good point is the curiosity. challenging to something new with your curiosity looks attractive to others',
-    '{userName}`s good point is the caring about others.You may not notice but your action is helping people meantaly',
-    '{userName}`s good point is eveything youve got.Just be true to yourself and one advice i can give to you is to confess to your crush!! GOOD LUCK!',
-    '{userName}`s good point is the self-command. Everyone evaluates you in a good way!',
+const answers = [
+  '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
+  '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
+  '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
+  '{userName}のいいところは厳しさです。{userName}の厳しさがものごとをいつも成功に導きます。',
+  '{userName}のいいところは知識です。博識な{userName}を多くの人が頼りにしています。',
+  '{userName}のいいところはユニークさです。{userName}だけのその特徴が皆を楽しくさせます。',
+  '{userName}のいいところは用心深さです。{userName}の洞察に、多くの人が助けられます。',
+  '{userName}のいいところは見た目です。内側から溢れ出る{userName}の良さに皆が気を惹かれます。',
+  '{userName}のいいところは決断力です。{userName}がする決断にいつも助けられる人がいます。',
+  '{userName}のいいところは思いやりです。{userName}に気をかけてもらった多くの人が感謝しています。',
+  '{userName}のいいところは感受性です。{userName}が感じたことに皆が共感し、わかりあうことができます。',
+  '{userName}のいいところは節度です。強引すぎない{userName}の考えに皆が感謝しています。',
+  '{userName}のいいところは好奇心です。新しいことに向かっていく{userName}の心構えが多くの人に魅力的に映ります。',
+  '{userName}のいいところは気配りです。{userName}の配慮が多くの人を救っています。',
+  '{userName}のいいところはその全てです。ありのままの{userName}自身がいいところなのです。',
+  '{userName}のいいところは自制心です。やばいと思ったときにしっかりと衝動を抑えられる{userName}が皆から評価されています。',
+  '{userName}のいいところは優しさです。{userName}の優しい雰囲気や立ち振る舞いに多くの人が癒やされています。'
 ];
 
 /**
